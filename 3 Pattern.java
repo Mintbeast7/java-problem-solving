450185c2d0a17f682354eaff850bdf46c3bc0677
Mintbeast7 <bhattacharjeebidyut07@gmail.com>
2026-04-19 20:44:01 +0530

    Add: Pattern program

---
+++ 3 Pattern.java
@@ -0,0 +1,25 @@
+class Pattern {
+    public static void main(String[] args) {
+    //Right Half Pyramid
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+    //Reverse Right Half Pyramid
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+
+    //Left Half Pyramid
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+
+    }
+    }
